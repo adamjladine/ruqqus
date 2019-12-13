e457cfd076b08309608162a6986ba064a34aafde
captainmeta4 <captainmeta4@gmail.com>
2019-12-13 16:17:35 -0500

    Update ios.js

--- ruqqus/assets/js/ios.js
+++ ruqqus/assets/js/ios.js
@@ -1,20 +1,15 @@
-// by https://github.com/irae
-(function(document,navigator,standalone) {
-    // prevents links from apps from oppening in mobile safari
-    // this javascript must be the first script in your <head>
-    if ((standalone in navigator) && navigator[standalone]) {
-        var curnode, location=document.location, stop=/^(a|html)$/i;
-        document.addEventListener('click', function(e) {
-            curnode=e.target;
-            while (!(stop).test(curnode.nodeName)) {
-                curnode=curnode.parentNode;
-            }
-            // Condidions to do this only on links to your own app
-            // if you want all links, use if('href' in curnode) instead.
-            if('href' in curnode && ( curnode.href.indexOf('http') || ~curnode.href.indexOf(location.host) ) ) {
-                e.preventDefault();
-                location.href = curnode.href;
-            }
-        },false);
+$( document ).on(
+    "click",
+    "a",
+    function( event ){
+
+        // Stop the default behavior of the browser, which
+        // is to change the URL of the page.
+        event.preventDefault();
+
+        // Manually change the location of the page to stay in
+        // "Standalone" mode and change the URL at the same time.
+        location.href = $( event.target ).attr( "href" );
+
     }
-})(document,window.navigator,'standalone');
+);
